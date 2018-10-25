73d36a7c46d88e2e79be8edf56f8250c57330084
wuling31715 <wuling31715@gmail.com>
2018-10-25 20:38:31 +0800

    save

--- application.py
+++ application.py
@@ -8,49 +8,49 @@ port = random.randrange(1000, 9999)
 def index():
     return render_template(
         'index.html', 
-        title = 'Hello World!',
+        title = 'Drunk.AI',
         )
 
 @application.route("/index.html")
 def home():
     return render_template(
         'index.html', 
-        title = 'Hello World!',
+        title = 'Drunk.AI',
         )
 
 @application.route("/widgets.html")
 def widgets():
     return render_template(
         'widgets.html', 
-        title = 'Hello World!',
+        title = 'Drunk.AI',
         )
 
 @application.route("/charts.html")
 def charts():
     return render_template(
         'charts.html', 
-        title = 'Hello World!',
+        title = 'Drunk.AI',
         )        
 
 @application.route("/elements.html")
 def elements():
     return render_template(
         'elements.html', 
-        title = 'Hello World!',
+        title = 'Drunk.AI',
         )        
 
 @application.route("/panels.html")
 def panels():
     return render_template(
         'panels.html', 
-        title = 'Hello World!',
+        title = 'Drunk.AI',
         )        
 
 @application.route("/login.html")
 def login():
     return render_template(
         'login.html', 
-        title = 'Hello World!',
+        title = 'Drunk.AI',
         )        
 
 
@@ -58,6 +58,7 @@ def login():
 def query():
     return render_template(
         'index.html', 
+        title = 'Drunk.AI'
         )
 
 if __name__ == "__main__":
