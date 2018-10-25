b2d52f5295d9f98653be4045add294e6b2683b69
wuling31715 <wuling31715@gmail.com>
2018-10-25 20:10:26 +0800

    application

--- application.py
+++ application.py
@@ -1,52 +1,52 @@
 from flask import Flask, render_template, redirect, request
 import random
 
-app = Flask(__name__)
+application = Flask(__name__)
 port = random.randrange(1000, 9999)
 
-@app.route("/")
+@application.route("/")
 def index():
     return render_template(
         'index.html', 
         title = 'Hello World!',
         )
 
-@app.route("/index.html")
+@application.route("/index.html")
 def home():
     return render_template(
         'index.html', 
         title = 'Hello World!',
         )
 
-@app.route("/widgets.html")
+@application.route("/widgets.html")
 def widgets():
     return render_template(
         'widgets.html', 
         title = 'Hello World!',
         )
 
-@app.route("/charts.html")
+@application.route("/charts.html")
 def charts():
     return render_template(
         'charts.html', 
         title = 'Hello World!',
         )        
 
-@app.route("/elements.html")
+@application.route("/elements.html")
 def elements():
     return render_template(
         'elements.html', 
         title = 'Hello World!',
         )        
 
-@app.route("/panels.html")
+@application.route("/panels.html")
 def panels():
     return render_template(
         'panels.html', 
         title = 'Hello World!',
         )        
 
-@app.route("/login.html")
+@application.route("/login.html")
 def login():
     return render_template(
         'login.html', 
@@ -54,11 +54,11 @@ def login():
         )        
 
 
-@app.route("/query", methods=['POST', 'GET'])
+@application.route("/query", methods=['POST', 'GET'])
 def query():
     return render_template(
         'index.html', 
         )
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=port)
+    application.run(debug=True, host='0.0.0.0', port=port)
